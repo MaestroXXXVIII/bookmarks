1c93011c0c25305d80117ae736dc8772fa647365
MaestroXXXVIII <urmanov.2000@mail.ru>
2023-09-14 19:37:39 +0800

    Добавлена возможность регистрации пользователя

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,20 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    repeated_password = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'email']
+
+    def clean_repeated_password(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['repeated_password']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['repeated_password']
--- account/views.py
+++ account/views.py
@@ -2,14 +2,35 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            # сначала хешируем пароль для безопасности, потом сохраняем
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request,
+                          'account/register_done.html',
+                          {'user_form': user_form})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                  'account/register.html',
+                  {'user_form': user_form})
+
 
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(request, username=cd['username'], password=cd['password'])
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
         if user is not None:
             if user.is_active:
                 login(request, user)
@@ -20,10 +41,14 @@ def user_login(request):
             return  HttpResponse('Invalid login')
     else:
         form=LoginForm()
-    return render(request, 'account/login.html', {'form': form})
+    return render(request,
+                  'account/login.html',
+                  {'form': form})
 
 
 # проверка аутентификации текущего пользователя
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
