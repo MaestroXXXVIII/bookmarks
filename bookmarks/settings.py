e9c88e15c68208be933f72add6f83395eae9a1a8
MaestroXXXVIIII <urmanov.pro@gmail.com>
2023-09-25 19:16:38 +0800

    Добавление возможности делиться изображениями с других сайтов на сайте bookmarks

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'filer',
     'social_django',
     'django_extensions',
+    'images.apps.ImagesConfig',
 ]
 
 MIDDLEWARE = [
---
+++ images/views.py
@@ -0,0 +1,25 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+
+from .forms import ImageCreateForm
+
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        form = ImageCreateForm(request.POST)
+        if form.is_valid():
+            new_image = form.save(commit=False)
+            print(new_image)
+            new_image.user = request.user
+            new_image.save()
+            messages.success(request,
+                             'Image added successfully')
+            return redirect(new_image.get_absolute_url())
+    else:
+        form = ImageCreateForm(data=request.GET)
+    return render(request,
+                  'images/image/create.html',
+                  {'section': 'imgaes',
+                   'form': form})
