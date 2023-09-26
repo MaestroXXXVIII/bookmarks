df310cc1c6ba24e37f17666475f27f845bd334eb
MaestroXXXVIIII <urmanov.pro@gmail.com>
2023-09-26 17:20:35 +0800

    Добавление возможности лайков

--- images/models.py
+++ images/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils.text import slugify
 
 
@@ -25,8 +26,11 @@ class Image(models.Model):
 
     def __str__(self):
         return self.title
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("images:detail", args=[self.id, self.slug])
